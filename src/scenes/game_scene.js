6ca2e17ea5c5d7983003479d6aaf8875574efc03
Dani <dangp2003@gmail.com>
2025-06-12 17:15:18 +0200

    .

--- src/scenes/game_scene.js
+++ src/scenes/game_scene.js
@@ -6,17 +6,21 @@ export default class GameScene extends Phaser.Scene
     create(){
         // Crea la instancia del huevo y le añade físicas
         const huevo = this.add.circle(400, 250, 10, 0xffffff, 1)
-
+        this.huevo_count = this.add.text(0, 20, "Huevos: ")
         // Crea la instancia de la huevera
         const huevera = this.add.rectangle(400, 400, 10, 5, 0xffffff, 1)
 
         this.physics.add.existing(huevo)
         this.physics.add.existing(huevera, true) // Vuelve la huevera estática
 
+        // Colisiones
+        this.physics.add.collider(huevo, hueveram, this.addHuevo)
+
         this.huevos = 0
     }
 
     addHuevo(){
-
+        this.huevos += 1
+        this.huevo_count.setText("Huevos: " + (this.huevos - '0'))
     }
 }
