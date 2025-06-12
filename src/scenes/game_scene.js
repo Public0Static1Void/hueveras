2637c5075e5b24a04e94eefa9a120c3f6ae59ca7
Dani <dangp2003@gmail.com>
2025-06-12 19:55:15 +0200

    Prueba hueveras

--- src/scenes/game_scene.js
+++ src/scenes/game_scene.js
@@ -11,58 +11,32 @@ export default class GameScene extends Phaser.Scene
         this.playing = true
 
         // Fondo
-        this.add.image(400, 250, "background").setDepth(-2).setDisplaySize(800, 500)
-        this.add.image(0, 250, "bg_paja").setDepth(-1).setDisplaySize(200, 250)
-        
-        // Crea la instancia del huevo y le añade físicas
-        this.huevo_count = this.add.text(0, 20, "Huevos: ")
+        this.add.image(800, 500, "background").setDepth(-2).setDisplaySize(800, 500)
+        this.add.image(0, 250, "bg_paja").setDepth(-1).setDisplaySize(200, 500)
 
-        // Crea la instancia de la huevera
-        this.huevera = this.add.rectangle(400, 400, 10, 5, 0xffffff, 1)
+        this.spawnHuevera(50, 150, 0xffffff)
+        this.spawnHuevera(50, 300, 0x310e08)
+        this.spawnHuevera(50, 450, 0xC7A144)
+    }
 
-        this.physics.add.existing(this.huevera, true) // Vuelve la huevera estática
+    spawnHuevera(x, y, color, type){
+        const huevera = this.add.image(x, y, 'huevera')
+        huevera.setTint(color)
+        huevera.setData('type', type)
+    }
+    spawnHuevo(x, y, color, type){
+        const huevo = this.add.image(x, y, 'huevo_base').setInteractive()
+        huevo.setTint(color)
+        huevo.setData('type', type)
 
-        this.huevosGroup = this.physics.add.group()
-
-        this.spawnHuevo()
-
-        this.huevos = 0
-
-        // Hace el mapeado de inputs
-        this.keys = this.input.keyboard.createCursorKeys()
+        huevo.on('pointerdown', () => {
+            console.log(`Color huevo: ${huevo.getData('color')}`)
+        })
     }
 
     update(){
         if (!this.playing) return
 
-        const huevoChildren = this.huevosGroup.getChildren()
-        for (let i = 0; i < huevoChildren.length; i++){
-            if (huevoChildren[i].y > this.sys.game.config.height + 50){
-                huevoChildren[i].destroy()
-            }
-        }
-    }
-
-    spawnHuevo(){
-        const rand_x = Phaser.Math.Between(0, 800)
         
-        const huevo = this.add.circle(rand_x, 0, 10, 0xffffff, 1)
-        this.physics.add.existing(huevo)
-        huevo.body.setVelocityY(150)
-
-        this.huevosGroup.add(huevo)
-
-        // Colisiones
-        this.physics.add.collider(huevo, this.huevera, () => {
-            this.addHuevo(huevo)
-        })
-    }
-
-    addHuevo(huevo){
-        this.huevos += 1
-        this.huevo_count.setText("Huevos: " + (this.huevos - '0'))
-
-        huevo.destroy()
-    }
-    
+    } 
 }
